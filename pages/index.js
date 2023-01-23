a1e759d88da630fda2dd6642f26742cd43872bb8
AcRyu01 <29421achira@gmail.com>
2023-01-23 13:04:57 +0700

    create Banner component

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,14 @@
 import Head from "next/head";
 import Image from "next/image";
+import Banner from "@/components/Banner/Banner";
 
 export default function Home() {
   return (
     <>
-      <h1 className="mt-20 text-[48px] font-bold text-red-500 text-center">
+      {/* <h1 className="mt-20 text-[48px] font-bold text-red-500 text-center">
         Hello World!
-      </h1>
+      </h1> */}
+      <Banner />
     </>
   );
 }
