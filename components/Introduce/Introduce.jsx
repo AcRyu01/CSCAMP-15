9ea7a97f770d8d5e97523d488229e8c9db00127a
AcRyu <73841516+AcRyu01@users.noreply.github.com>
2023-01-26 18:19:37 +0700

    Ryu introduce v.1 (#3)
    * Almost done
    
    * version 1 done
    
    * rename import wave

---
+++ components/Introduce/Introduce.jsx
@@ -0,0 +1,21 @@
+function Introduce() {
+  return (
+    <div className="flex flex-col items-center pt-4 md:pt-10 2xl:pt-20 mx-11 md:mx-24 2xl:mx-36 mb-64 md:mb-32 2xl:mb-56 text-center text-[#F2F2F2] ">
+      <h2 className="text-3xl md:text-4xl 2xl:text-8xl font-bold md:mb-12 2xl:mb-10">
+        แนะนำค่าย
+      </h2>
+      <p className="text-base md:text-xl 2xl:text-4xl font-normal">
+        CS Camp 15
+      </p>
+      <p className="text-base md:text-xl 2xl:text-4xl font-normal md:max-w-[700px] 2xl:max-w-[1000px]">
+        เป็นค่ายอมรมเชิงปฏิบัติการวิทยาการคอมพิวเตอร์ ที่จะให้เหล่าน้อง ๆ
+        นักเรียนที่สนใจอยากจะท่องโลกและเรียนรู้ เกี่ยวกับวิทยาการคอมพิวเตอร์
+        ซึ่งในค่ายจะประกอบไปด้วยสาระ และความสนุกมากมาย
+        โดยค่ายจัดขึ้นโดยคณะวิทยาศาสตร์ สาขาวิทยาการคอมพิวเตอร์
+        สถาบันเทคโนโลยีพระจอมเกล้าเจ้าคุณทหารลาดกระบัง
+      </p>
+    </div>
+  );
+}
+
+export default Introduce;
