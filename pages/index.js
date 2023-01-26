b360b3bb540da7d68ee761bddb072dd5b0156fe3
Emotions <estskywalker@gmail.com>
2023-01-26 21:19:58 +0700

    Added shedule scroll animation

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,15 @@
 import Head from "next/head";
 import Image from "next/image";
 import Banner from "@/components/Banner/Banner";
+import Shedule from "@/components/Schedule/shedule";
+
+import { ScrollContainer, ScrollPage } from "react-scroll-motion";
 
 export default function Home() {
   return (
     <>
-      <Banner />
+          <Banner />
+          <Shedule />
     </>
   );
 }
