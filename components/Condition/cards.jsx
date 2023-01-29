144828eb8e8e5a676fe078ebff7d6ffe8e1fccba
Emotions <estskywalker@gmail.com>
2023-01-29 16:02:49 +0700

    added Condition page

---
+++ components/Condition/cards.jsx
@@ -0,0 +1,11 @@
+import FCard1 from "@/assets/images/Condition/FCard1.png"
+import FCard2 from "@/assets/images/Condition/FCard2.png"
+import FCard3 from "@/assets/images/Condition/Fcard3.png"
+import BCard1 from "@/assets/images/Condition/BCard1.png"
+import BCard2 from "@/assets/images/Condition/BCard2.png"
+import BCard3 from "@/assets/images/Condition/Bcard3.png"
+
+const fCards = [FCard1, FCard2, FCard3]
+const bCards = [BCard1, BCard2, BCard3]
+
+export default { fCards, bCards }
--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import Head from "next/head";
 import Image from "next/image";
 import Banner from "@/components/Banner/Banner";
 import Introduce from "@/components/Introduce/Introduce";
+import Condition from "@/components/Condition/Condition";
 
 export default function Home() {
   return (
@@ -10,6 +11,7 @@ export default function Home() {
       <div className="bg-content">
         <Introduce />
         <div className="h-60 text-center text-white">เงื่อนไข dummy</div>
+        <Condition />
       </div>
     </>
   );
