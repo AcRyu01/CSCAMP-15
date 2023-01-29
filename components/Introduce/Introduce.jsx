fdeeb2879c7d7de971fd8cae7ab8dec01a778350
AcRyu01 <29421achira@gmail.com>
2023-01-29 22:14:35 +0700

    fix some style

--- components/Introduce/Introduce.jsx
+++ components/Introduce/Introduce.jsx
@@ -5,21 +5,25 @@ function Introduce() {
       className="flex flex-col items-center pt-4 md:pt-10 2xl:pt-20 mx-11 md:mx-24 2xl:mx-36 mb-64 md:mb-32 2xl:mb-56 text-center text-[#F2F2F2] "
     >
       <h2 className="topic mb-2 md:mb-12 2xl:mb-10">CS CAMP 15</h2>
-      <p className="text-base leading-7 md:leading-9 2xl:leading-10 md:text-xl 2xl:text-4xl font-normal md:max-w-[700px] 2xl:max-w-[1000px]">
+      <p className="text-base tracking-wide leading-7 md:leading-9 2xl:leading-[50px] md:text-xl 2xl:text-4xl font-extralight md:max-w-[700px] 2xl:max-w-[1000px]">
         ค่ายที่เปิดโอกาสให้โจรสลัดผู้กล้าทั้งหลายที่สนใจในด้านวิทยาการคอมพิวเตอร์
         ที่พร้อมจะออกไปตามล่าหาขุมทรัพย์ที่ซ่อนอยู่ตามเกาะต่าง ๆ
         โดยต้องการผู้กล้าที่พร้อมจะเผชิญหน้ากับความอันตรายในการเดินเรือครั้งนี้!
         แม้ว่าจะมีปลาหมึกยักษ์น่าสะพรึงกลัว
         ที่พร้อมจะคร่าชีวิตเหล่าโจรสลัดได้ทุกเมื่อ และคลื่นลมกำลังแรง
         ที่ทำให้เรือโคลงเคลงขนาดไหน เราก็จะใช้อัลกอริทึมอันชาญฉลาด
-        เพื่อเอาชีวิตรอดกลับมาโดยให้จงได้!!
+        เพื่อเอาชีวิตรอดกลับมาให้ได้!!
       </p>
       <br />
       <br />
-      <p className="text-base leading-7 md:leading-9 2xl:leading-10 md:text-xl 2xl:text-4xl font-semibold md:max-w-[700px] 2xl:max-w-[1000px]">
-        พร้อมที่จะออกไปตามล่าหาสมบัติกับเราหรือยัง
-        เหล่าโจรสลัดหน้าใหม่ทั้งหลาย...
-      </p>
+      <div>
+        <p className="text-base tracking-wide leading-7 md:leading-9 2xl:leading-[50px] md:text-xl 2xl:text-4xl font-normal md:max-w-[700px] 2xl:max-w-[1000px]">
+          พร้อมที่จะออกไปตามล่าหาสมบัติกับเราหรือยัง
+        </p>
+        <p className="text-base tracking-wide leading-7 md:leading-9 2xl:leading-[50px] md:text-xl 2xl:text-4xl font-normal md:max-w-[700px] 2xl:max-w-[1000px]">
+          เหล่าโจรสลัดหน้าใหม่ทั้งหลาย...
+        </p>
+      </div>
     </div>
   );
 }
--- pages/_document.js
+++ pages/_document.js
@@ -1,13 +1,15 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <link rel="shortcut icon" type="image/png" href="/LogoCamp.png" />
+      </Head>
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
