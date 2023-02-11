0200f68267c1d4c49f7a8acce48dad55f537be3a
Emotions <estskywalker@gmail.com>
2023-02-11 16:04:38 +0700

    [Added] add introduce and card animation

--- components/Introduce/Introduce.jsx
+++ components/Introduce/Introduce.jsx
@@ -1,10 +1,46 @@
+import { useEffect, useRef } from "react";
+import { motion, useAnimationControls, useInView } from "framer-motion";
+
+const containerVariants = {
+  hidden: {
+    opacity: 0,
+    y: 100,
+  },
+  visible: {
+    opacity: 1,
+    y: 0,
+    transition: {
+      duration: 0.6,
+      type: "tween",
+    },
+  },
+};
+
 function Introduce() {
+
+  const ref = useRef(null);
+  const isInView = useInView(ref, { once: true });
+  const control = useAnimationControls();
+
+  useEffect(() => {
+    if (isInView) {
+      control.start("visible");
+    } else {
+      control.set("hidden");
+    }
+  }, [isInView]);
+
   return (
-    <div
+    <motion.div
       id="intro"
       className="flex flex-col items-center mt-8 pt-4 md:pt-10 2xl:pt-20 mx-11 md:mx-24 2xl:mx-36 mb-64 md:mb-32 2xl:mb-56 text-center text-[#F2F2F2] "
+      variants={containerVariants}
+      initial={"hidden"}
+      animate={control}
     >
-      <h2 className="topic mb-2 md:mb-12 2xl:mb-10">CS CAMP 15</h2>
+      <h2 className="topic mb-2 md:mb-12 2xl:mb-10" ref={ref}>
+        CS CAMP 15
+      </h2>
       <p className="text-base tracking-wide leading-7 md:leading-9 2xl:leading-[50px] md:text-xl 2xl:text-4xl font-extralight md:max-w-[700px] 2xl:max-w-[1000px]">
         ค่ายที่เปิดโอกาสให้โจรสลัดผู้กล้าทั้งหลายที่สนใจในด้านวิทยาการคอมพิวเตอร์
         ที่พร้อมจะออกไปตามล่าหาขุมทรัพย์ที่ซ่อนอยู่ตามเกาะต่าง ๆ
@@ -24,7 +60,7 @@ function Introduce() {
           เหล่าโจรสลัดหน้าใหม่ทั้งหลาย...
         </p>
       </div>
-    </div>
+    </motion.div>
   );
 }
 
