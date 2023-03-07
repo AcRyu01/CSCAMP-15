4c99b6a9e5bc163212ee7d1823702a7c7ab28956
Emotions <estskywalker@gmail.com>
2023-03-07 18:54:25 +0700

    fixed bug

--- components/Banner/Banner.jsx
+++ components/Banner/Banner.jsx
@@ -236,12 +236,6 @@ function Banner() {
                   }}
                   key={index}
                 >
-                  <a
-                    href={links[bottles.indexOf(text)]}
-                    target="_blank"
-                    rel="noreferrer"
-                  >
-                >
                   <a href={links[bottles.indexOf(text)]} target="_blank" rel="noreferrer">
                     {text}
                   </a>
