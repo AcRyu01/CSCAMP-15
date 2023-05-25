2a6fdab3d07c8f614892200557c2f5ff1554ee09
AcRyu01 <29421achira@gmail.com>
2023-05-25 22:06:25 +0700

    disable next/font/local

--- pages/_app.js
+++ pages/_app.js
@@ -1,53 +1,54 @@
-import localFont from "@next/font/local";
+// import localFont from "@next/font/local";
 // const localFont = require("@next/font/local");
 import Layout from "@/components/Layout/Layout";
 import "@/styles/globals.css";
 
-const sukhumvitSet = localFont({
-  src: [
-    {
-      path: "../assets/fonts/SukhumvitSet-Thin.ttf",
-      weight: "100",
-    },
-    {
-      path: "../assets/fonts/SukhumvitSet-Light.ttf",
-      weight: "300",
-    },
-    {
-      path: "../assets/fonts/SukhumvitSet-Text.ttf",
-      weight: "400",
-    },
-    {
-      path: "../assets/fonts/SukhumvitSet-Medium.ttf",
-      weight: "500",
-    },
-    {
-      path: "../assets/fonts/SukhumvitSet-SemiBold.ttf",
-      weight: "600",
-    },
-    {
-      path: "../assets/fonts/SukhumvitSet-Bold.ttf",
-      weight: "700",
-    },
-  ],
-  variable: "--font-sukhumvitSet",
-});
+// const sukhumvitSet = localFont({
+//   src: [
+//     {
+//       path: "../assets/fonts/SukhumvitSet-Thin.ttf",
+//       weight: "100",
+//     },
+//     {
+//       path: "../assets/fonts/SukhumvitSet-Light.ttf",
+//       weight: "300",
+//     },
+//     {
+//       path: "../assets/fonts/SukhumvitSet-Text.ttf",
+//       weight: "400",
+//     },
+//     {
+//       path: "../assets/fonts/SukhumvitSet-Medium.ttf",
+//       weight: "500",
+//     },
+//     {
+//       path: "../assets/fonts/SukhumvitSet-SemiBold.ttf",
+//       weight: "600",
+//     },
+//     {
+//       path: "../assets/fonts/SukhumvitSet-Bold.ttf",
+//       weight: "700",
+//     },
+//   ],
+//   variable: "--font-sukhumvitSet",
+// });
 
 export default function App({ Component, pageProps }) {
   return (
     <>
-      <main className={`${sukhumvitSet.variable} font-primary`}>
+      {/* <main className={`${sukhumvitSet.variable} font-primary`}> */}
+      <main className={`font-primary`}>
         <Layout>
           <Component {...pageProps} />
         </Layout>
       </main>
-      <style jsx global>
+      {/* <style jsx global>
         {`
           :root {
             --font-sukhumvitSet: ${sukhumvitSet.variable};
           }
         `}
-      </style>
+      </style> */}
     </>
   );
 }
